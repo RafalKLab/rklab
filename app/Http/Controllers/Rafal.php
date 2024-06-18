c25bd2d648eccb4a6e6f69c4396703d092a15cf2
RKLAB <kadzewiczrafal@gmail.com>
2024-06-18 23:51:15 +0300

    Add counter

--- app/Http/Controllers/Rafal.php
+++ app/Http/Controllers/Rafal.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PageCounter;
 use Illuminate\Http\Request;
+use SebastianBergmann\LinesOfCode\Counter;
 
 class Rafal extends Controller
 {
@@ -13,6 +15,20 @@ class Rafal extends Controller
 
     public function roll()
     {
-        return view('Rafal.roll');
+        // Attempt to find the counter by ID, or create a new one if it doesn't exist
+        $counter = PageCounter::firstOrCreate(
+            ['id' => 1], // The attributes to search for
+            ['count' => 0] // The default values for creation
+        );
+
+        // Increment the count
+        $counter->increment('count');
+
+        // Save the changes to the database
+        $counter->save();
+
+        $amount = $counter->count;
+
+        return view('Rafal.roll', compact('amount'));
     }
 }
--- resources/views/Rafal/roll.blade.php
+++ resources/views/Rafal/roll.blade.php
@@ -7,6 +7,7 @@
 </head>
 <body>
 <h1>Enjoy the Video!</h1>
+<h4>Views {{ $amount }}</h4>
 <iframe width="1903" height="790" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="Rick Astley - Never Gonna Give You Up (Official Music Video)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 </body>
 </html>
