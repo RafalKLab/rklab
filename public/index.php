bfabdb4b33208bbe459872a362e8924cdbd097ef
RKLAB <kadzewiczrafal@gmail.com>
2024-07-07 00:23:54 +0300

    Add ekimex product pages

--- public/index.php
+++ public/index.php
@@ -1,17 +1,376 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-use Illuminate\Http\Request;
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-define('LARAVEL_START', microtime(true));
+    <title>Ekimex OU</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
+    <!-- Favicons -->
 
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
-// Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
-    ->handleRequest(Request::capture());
+    <!-- Vendor CSS Files -->
+    <link href="./ekimex/assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="./ekimex/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="./ekimex/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="./ekimex/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="./ekimex/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="./ekimex/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="./ekimex/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="./ekimex/assets/css/style.css" rel="stylesheet">
+
+    <!-- =======================================================
+    * Template Name: Bethany
+    * Template URL: https://bootstrapmade.com/bethany-free-onepage-bootstrap-theme/
+    * Updated: Mar 17 2024 with Bootstrap v5.3.3
+    * Author: BootstrapMade.com
+    * License: https://bootstrapmade.com/license/
+    ======================================================== -->
+
+
+</head>
+
+<body>
+
+<!-- ======= Header ======= -->
+<header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container">
+        <div class="header-container d-flex align-items-center justify-content-between">
+            <div class="logo">
+                <a class="scrollto" href="#hero"><img src="./ekimex/assets/img/Monochrome on transparent.png" alt="" class="img-fluid"></a>
+            </div>
+
+            <nav id="navbar" class="navbar">
+                <ul>
+                    <li><a class="nav-link scrollto active" href="#hero">HOME</a></li>
+                    <li><a class="nav-link scrollto" href="#about">ABOUT US</a></li>
+                    <li><a class="nav-link scrollto" href="#services">PRODUCTS</a></li>
+                    <li><a class="scrollto" href="#contact">Contact</a></li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav><!-- .navbar -->
+
+        </div><!-- End Header Container -->
+    </div>
+</header><!-- End Header -->
+
+<!-- ======= Hero Section ======= -->
+<section id="hero" class="d-flex align-items-center">
+    <div class="container text-center position-relative" data-aos="fade-in" data-aos-delay="200">
+        <h1>Wholesale Wood Products</h1>
+        <h2>Providing high-quality wood products for construction, manufacturing, and trading companies.</h2>
+        <a href="#about" class="btn-get-started scrollto">Explore</a>
+    </div>
+</section><!-- End Hero -->
+
+<main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+        <div class="container">
+
+            <div class="row content">
+                <div class="col-lg-6" data-aos="fade-right" data-aos-delay="100">
+                    <h2>About us</h2>
+                    <h3>We are a leading wholesale and retail trade company specializing in wood products.</h3>
+                </div>
+                <div class="col-lg-6 pt-4 pt-lg-0" data-aos="fade-left" data-aos-delay="200">
+                    <p>
+                        We are a leading wholesale and retail trade company specializing in high-quality wood products. With a commitment to excellence and customer satisfaction, we provide a diverse range of timber and wood-related materials to meet the needs of various industries and individual customers.
+
+                    </p>
+                    <ul>
+                        <li><i class="ri-check-double-line"></i> Certified Production</li>
+                        <li><i class="ri-check-double-line"></i> Diverse Product Range</li>
+                        <li><i class="ri-check-double-line"></i> Sustainable Practices</li>
+                    </ul>
+                    <p class="fst-italic">
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Why Us Section ======= -->
+    <section id="why-us" class="why-us">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-lg-4 d-flex align-items-stretch" data-aos="fade-right">
+                    <div class="content">
+                        <h3>Why Choose Ekimex for your business?</h3>
+                        <p>
+                            As a premier wholesale and retail trade company, we specialize in offering an extensive selection of premium wood products. Our dedication to quality and service ensures that we meet the diverse needs of both industry professionals and individual customers, delivering superior timber and wood materials for every project.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg-8 d-flex align-items-stretch">
+                    <div class="icon-boxes d-flex flex-column justify-content-center">
+                        <div class="row">
+                            <div class="col-xl-4 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+                                <div class="icon-box mt-4 mt-xl-0">
+                                    <i class="bx bx-check"></i>
+                                    <h4>Commitment to Quality</h4>
+                                    <p>We prioritize excellence in every product and service, ensuring our wood materials meet the highest standards for durability and aesthetic appeal.</p>
+                                </div>
+                            </div>
+                            <div class="col-xl-4 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="200">
+                                <div class="icon-box mt-4 mt-xl-0">
+                                    <i class="bx bxs-user"></i>
+                                    <h4>Customer Satisfaction</h4>
+                                    <p>We strive to exceed customer expectations through personalized service, attentive support, and reliable delivery, building long-term relationships based on trust and satisfaction.</p>
+                                </div>
+                            </div>
+                            <div class="col-xl-4 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="300">
+                                <div class="icon-box mt-4 mt-xl-0">
+                                    <i class="bx bxs-tree-alt"></i>
+                                    <h4>Sustainability</h4>
+                                    <p>We are dedicated to responsible sourcing and environmental stewardship, ensuring that our wood products come from sustainably managed forests to preserve resources for future generations.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- End .content-->
+                </div>
+            </div>
+
+        </div>
+    </section><!-- End Why Us Section -->
+
+    <!-- ======= Cta Section ======= -->
+    <section id="cta" class="cta">
+        <div class="container">
+
+            <div class="text-center" data-aos="zoom-in">
+                <h3>Building with Nature, Building with Ekimex</h3>
+            </div>
+
+        </div>
+    </section><!-- End Cta Section -->
+
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services section-bg">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-lg-8">
+                    <div class="section-title" data-aos="fade-right">
+                        <h2>PRODUCTS</h2>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row justify-content-center">
+                <!-- Product 1 -->
+
+                    <div class="col-md-3 d-flex align-items-stretch mt-4">
+                        <div class="icon-box" data-aos="zoom-in" data-aos-delay="100">
+                            <div class="icon-box-product-image">
+                                <img src="./ekimex/assets/img/Birch plywood.jpg" alt="">
+                            </div>
+                            <a href="./birch-plywood.html">
+                              <h5 class="mt-2">Birch plywood</h5>
+                            </a>
+                        </div>
+                    </div>
+
+                <!-- Product 2 -->
+
+                    <div class="col-md-3 d-flex align-items-stretch mt-4">
+                        <div class="icon-box" data-aos="zoom-in" data-aos-delay="200">
+                            <div class="icon-box-product-image">
+                                <img src="./ekimex/assets/img/Film Faced Plywood.jpg" alt="">
+                            </div>
+                            <a href="./film-faced-plywood.html">
+                                <h5 class="mt-2">Film Faced plywood</h5>
+                            </a>
+                        </div>
+                    </div>
+
+                <!-- Product 3 -->
+                <div class="col-md-3 d-flex align-items-stretch mt-4">
+                    <div class="icon-box" data-aos="zoom-in" data-aos-delay="300">
+                        <div class="icon-box-product-image">
+                            <img src="./ekimex/assets/img/Film Faced Colored Plywood2.jpg" alt="">
+                        </div>
+                        <a href="./film-faced-colored-plywood.html">
+                            <h5 class="mt-2">Film Faced Colored Plywood</h5>
+                        </a>
+
+                    </div>
+                </div>
+                <!-- Product 4 -->
+                <div class="col-md-3 d-flex align-items-stretch mt-4">
+                    <div class="icon-box" data-aos="zoom-in" data-aos-delay="400">
+                        <div class="icon-box-product-image">
+                            <img src="./ekimex/assets/img/Coniferous Plywood PineSpruce.jpg" alt="">
+                        </div>
+                        <a href="./coniferous-plywood-pine.html">
+                            <h5 class="mt-2">Coniferous Plywood Pine/Spruce</h5>
+                        </a>
+
+                    </div>
+                </div>
+                <!-- Product 5 -->
+                <div class="col-md-3 d-flex align-items-stretch mt-4">
+                    <div class="icon-box" data-aos="zoom-in" data-aos-delay="100">
+                        <div class="icon-box-product-image">
+                            <img src="./ekimex/assets/img/glued laminated timber.jpg" alt="">
+                        </div>
+                        <a href="./glued-laminated-timber.html">
+                            <h5 class="mt-2">Glued laminated timber</h5>
+                        </a>
+
+                    </div>
+                </div>
+                <!-- Product 6 -->
+                <div class="col-md-3 d-flex align-items-stretch mt-4">
+                    <div class="icon-box" data-aos="zoom-in" data-aos-delay="200">
+                        <div class="icon-box-product-image">
+                            <img src="./ekimex/assets/img/cross laminated timber.jpg" alt="">
+                        </div>
+                        <a href="./cross-laminated-timber.html">
+                            <h5 class="mt-2">Cross laminated timber</h5>
+                        </a>
+
+                    </div>
+                </div>
+                <!-- Product 7 -->
+                <div class="col-md-3 d-flex align-items-stretch mt-4">
+                    <div class="icon-box" data-aos="zoom-in" data-aos-delay="300">
+                        <div class="icon-box-product-image">
+                            <img src="./ekimex/assets/img/Three-layer panels (SWP).jpg" alt="">
+                        </div>
+                        <a href="./three-layer-panels.html">
+                            <h5 class="mt-2">Three-layer panels (SWP)</h5>
+                        </a>
+                    </div>
+                </div>
+                <!-- Product 8 -->
+                <div class="col-md-3 d-flex align-items-stretch mt-4">
+                    <div class="icon-box" data-aos="zoom-in" data-aos-delay="400">
+                        <div class="icon-box-product-image">
+                            <img src="./ekimex/assets/img/Bed slats.jpg" alt="">
+                        </div>
+                        <a href="./bed-slats.html">
+                            <h5 class="mt-2">Bed slats</h5>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section><!-- End Services Section -->
+
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4" data-aos="fade-right">
+                    <div class="section-title">
+                        <h2>Contact</h2>
+                        <p></p>
+                    </div>
+                </div>
+
+                <div class="col-lg-8" data-aos="fade-up" data-aos-delay="100">
+                    <iframe style="border:0; width: 100%; height: 270px;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2029.01449051709!2d24.75814017770205!3d59.432832874664584!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4692935f7e673a63%3A0xc9434eaf798bee02!2sTornim%C3%A4e%207%2C%2010145%20Tallinn%2C%20Estija!5e0!3m2!1slt!2slt!4v1720096550798!5m2!1slt!2slt" frameborder="0" allowfullscreen></iframe>
+                    <div class="info mt-4">
+                        <i class="bi bi-geo-alt"></i>
+                        <h4>Location:</h4>
+                        <p>Harju maakond, Tallinn, Kesklinna linnaosa, Tornimäe tn 7-26,
+                            10145,
+                            ESTONIA</p>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-6 mt-4">
+                            <div class="info">
+                                <i class="bi bi-envelope"></i>
+                                <h4>Email:</h4>
+                                <p>info@ekimex.com</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="info w-100 mt-4">
+                                <i class="bi bi-phone"></i>
+                                <h4>Call:</h4>
+                                <p>+1 1234567890</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </section><!-- End Contact Section -->
+
+</main><!-- End #main -->
+
+<!-- ======= Footer ======= -->
+<footer id="footer">
+
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-3 col-md-6 footer-contact">
+                    <h3>Ekimex OU</h3>
+                    <p>
+                        Harju maakond, Kesklinna linnaosa, <br>Tornimäe tn 7-26, 10145<br>
+                        Tallinn <br>
+                        ESTONIA <br><br>
+                        <strong>Phone:</strong> +1 1234567890<br>
+                        <strong>Email:</strong> info@ekimex.com<br>
+                    </p>
+                </div>
+
+                <div class="col-lg-2 col-md-6 footer-links">
+                    <h4>Useful Links</h4>
+                    <ul>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#hero">HOME</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#about">ABOUT US</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#services">PRODUCTS</a></li>
+                    </ul>
+                </div>
+                <div class="copyright">
+                    &copy; Copyright <strong><span>Ekimex OU</span></strong>. All Rights Reserved
+                </div>
+                <div class="credits">
+                    <!-- All the links in the footer should remain intact. -->
+                    <!-- You can delete the links only if you purchased the pro version. -->
+                    <!-- Licensing information: https://bootstrapmade.com/license/ -->
+                    <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/bethany-free-onepage-bootstrap-theme/ -->
+                    Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</footer><!-- End Footer -->
+
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+<!-- Vendor JS Files -->
+<script src="./ekimex/assets/vendor/purecounter/purecounter_vanilla.js"></script>
+<script src="./ekimex/assets/vendor/aos/aos.js"></script>
+<script src="./ekimex/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="./ekimex/assets/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="./ekimex/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="./ekimex/assets/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="./ekimex/assets/vendor/php-email-form/validate.js"></script>
+
+<!-- Template Main JS File -->
+<script src="./ekimex/assets/js/main.js"></script>
+
+</body>
+
+</html>
